2c601dfcaae60683b7f4c8dc29b7e52505902184
Seth McCullough <seth@endevrs.dev>
2023-01-02 12:12:21 -0500

    Update prettier to work with pnpm

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	printWidth: 120,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+	overrides: [
+		{
+			files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
+			options: {
+				useTabs: false,
+			},
+		},
+	],
+	plugins: [require.resolve('prettier-plugin-astro')],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+};
