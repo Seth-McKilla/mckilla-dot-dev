ed42805a470e2cf889bcc5d349a3e3005e6bf345
Seth McCullough <seth@endevrs.dev>
2023-01-18 08:05:16 -0500

    Fix site config url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-paper.pages.dev/',
+  site: 'https://mckilla.dev/',
   integrations: [
     tailwind({
       config: {
